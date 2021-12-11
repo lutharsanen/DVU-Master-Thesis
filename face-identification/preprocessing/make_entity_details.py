e7cc654d7aad02eb75ad710836cbcfdbf2dd849a
lutharsanen <lutharsanen.kunam@uzh.ch>
2021-12-11 19:32:01 +0100

    finished data processing pipeline

--- face-identification/preprocessing/make_entity_details.py
+++ face-identification/preprocessing/make_entity_details.py
@@ -1,15 +1,24 @@
 import os
 from tqdm import tqdm
+import json
 
-dir = "/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph"
+dir = "/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/Time Expired/TimeExpired.entity.types.txt"
 entity_type = {}
 
-for movies in tqdm(os.listdir(dir)):
-    if os.path.isdir(f"{dir}/{movies}"):
-        # detect type of entities
-        entity_type[movies] = {}
-        f = open(f"{dir}/{movies}/{movies}.entity.types.txt", "r").read()
-        lst = f.split("\n")
-        for element in lst[:-1]:
-           comp_lst = element.replace(" ","").split(":")
-           entity_type[movies][comp_lst[0]] = comp_lst[1]
+#for movies in tqdm(os.listdir(dir)):
+#        # detect type of entities
+#        entity_type[movies] = {}
+#        for file in os.listdir(f"{dir}/spiritualContact"):
+#            if file.endswith(".entity.types.txt"):
+f = open(f"{dir}", "r").read()
+lst = f.split("\n")
+print(lst)
+for element in lst[:-1]:
+   comp_lst = element.replace(" ","").split(":")
+   print(comp_lst[0],comp_lst[1])
+   entity_type[comp_lst[0]] = comp_lst[1]
+
+
+with open('result.json', 'w') as fp:
+    json.dump(entity_type, fp)
+
--- face-identification/preprocessing/preprocessing.py
+++ face-identification/preprocessing/preprocessing.py
@@ -1,12 +1,10 @@
 import os
 import shutil
 from tqdm import tqdm
+import json
+from distutils.dir_util import copy_tree
 
 dir = "/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph"
-movie_entity_details = "/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/honey/honey.entity.types.txt"
-
-f = open(movie_entity_details, "r").read()
-lst = f.split("\n")
 entity_type = {}
 
 
@@ -14,35 +12,48 @@ entity_type = {}
 for movies in tqdm(os.listdir(dir)):
     if os.path.isdir(f"{dir}/{movies}"):
         # detect type of entities
-        entity_type[movies] = {}
-        f = open(f"{dir}/{movies}/{movies}.entity.types.txt", "r").read()
-        lst = f.split("\n")
-        for element in lst[:-1]:
-           comp_lst = element.replace(" ","").split(":")
-           entity_type[movies][comp_lst[0]] = comp_lst[1]
+        if os.path.isdir(f"{dir}/{movies}"):
+        # detect type of entities
+            entity_type[movies] = {}
+            for file in os.listdir(f"{dir}/{movies}"):
+                if file.endswith(".entity.types.txt"):
+                    f = open(f"{dir}/{movies}/{file}", "r").read()
+                    lst = f.split("\n")
+                    if "" in lst:
+                        lst.remove("")
+                        print("Leerzeichen entfernt")
+                    for element in lst:
+                        if element != "":
+                            comp_lst = element.replace(" ","").split(":")
+                            entity_type[movies][comp_lst[0].replace("'","_")] = comp_lst[1]
 
         for file in os.listdir(f"{dir}/{movies}/image"):
-           # get all but the last 8 characters to remove
-           # the index number and extension
-           dir_name = file[:-6]
+            copy_tree(f"{dir}/{movies}/image",f"{dir}/{movies}/image_copy")
+            # get all but the last 8 characters to remove
+            # the index number and extension
+            print(file)
+            dir_name = file[:-6].replace(" ","").replace("'", "_")
+            print(movies,dir_name)
 
-           ent_type = entity_type[dir_name]
+            ent_type = entity_type[movies][dir_name]
 
-           print(f'dir_name: {dir_name}')
+            print(f'dir_name: {dir_name}')
 
-           dir_path = f"{dir}/{movies}/movie/{ent_type}/{dir_name}"
-           print(f'dir_path: {dir_path}')
+            dir_path = f"{dir}/{movies}/image/{ent_type}/{dir_name}"
+            print(f'dir_path: {dir_path}')
 
-           # check if directory exists or not yet
-           if not os.path.exists(dir_path):
-               os.makedirs(dir_path)
+            # check if directory exists or not yet
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
 
-           if os.path.exists(dir_path):
-               print(file)
-               file_path = f"{dir}/{movies}/image/{file}"
-               print(f'file_path: {file_path}')
+            if os.path.exists(dir_path):
+                print(file)
+                file_path = f"{dir}/{movies}/image/{file}"
+                print(f'file_path: {file_path}')
 
-               # move files into created directory
-               shutil.move(file_path, dir_path)
+                # move files into created directory
+                shutil.move(file_path, dir_path)
 
 
+with open('result.json', 'w') as fp:
+    json.dump(entity_type, fp)
