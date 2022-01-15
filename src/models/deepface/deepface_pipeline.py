09c1faa0fd5f1378a18ccb33f57d06afb0960112
lutharsanen <lutharsanen.kunam@uzh.ch>
2022-01-15 20:28:44 +0100

    extended deepface pipeline

--- src/models/deepface/deepface_pipeline.py
+++ src/models/deepface/deepface_pipeline.py
@@ -1,20 +1,54 @@
+import os
+from retinaface import RetinaFace
 from deepface import DeepFace
+from PIL import Image
+import shutil
 
+
+def enlargen_image(border):
+    border[0]-=20
+    border[1]-=20
+    border[2]+=20
+    border[3]+=20
+    return border
+
+
+img_path = "/media/lkunam/DVU-Challenge/HLVU/keyframes/shot_keyf"
+
+#for movies in os.listdir(img_path):
 backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface']
 
-person_images = "/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/honey/image_copy/Person/"
 
-test_images = "/media/lkunam/DVU-Challenge/HLVU/keyframes/shot_keyf/honey-11/shot_0005/shot_0005_img_1.jpg"
-
-##face detection and alignment
-#detected_face = DeepFace.detectFace(img_path = "img.jpg", detector_backend = backends[4])
-#
-##face verification
-#obj = DeepFace.verify(img1_path = "img1.jpg", img2_path = "img2.jpg", detector_backend = backends[4])
-
-#face recognition
-df = DeepFace.find(img_path = "/media/lkunam/DVU-Challenge/HLVU/keyframes/shot_keyf/honey-7/shot_0005/shot_0005_img_2.jpg", db_path = "/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/honey/image_copy/", detector_backend = backends[3], enforce_detection = False)
-
-print(df)
-#facial analysis
-#demography = DeepFace.analyze(img_path = "img4.jpg", detector_backend = backends[4])
+movies = "honey"
+shotlist = os.listdir(f"{img_path}/{movies}")
+orderedshotlist = [i.partition('-')[2] for i in shotlist]
+for i in range(len(shotlist)):
+    num = i+1
+    list_index = orderedshotlist.index(str(num))
+    shots = shotlist[list_index]
+    for shot in os.listdir(f"{img_path}/{movies}/{shots}"):
+        for image in os.listdir(f"{img_path}/{movies}/{shots}/{shot}"):
+            #print(image)
+            #print(f"{img_path}/{movies}/{shots}/{shot}/{image}")
+            resp = RetinaFace.detect_faces(f"{img_path}/{movies}/{shots}/{shot}/{image}")
+            #print(len(resp))
+            #print(resp)
+            if len(resp) > 0 and type(resp) == dict:
+                for face in resp:
+                    print(face)
+                    border = resp[face]["facial_area"]
+                    enlarged_border = enlargen_image(border)
+                    im = Image.open(f"{img_path}/{movies}/{shots}/{shot}/{image}")
+                    im1 = im.crop(border)
+                    im1.save("cropped.jpg")
+                    try:
+                        df = DeepFace.find(img_path = "cropped.jpg", db_path = f"/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/{movies}/image/Person/", detector_backend = backends[4])
+                        result = df["identity"][0]
+                        name = result[-17:].partition('/')[0]
+                        #name = "{img_path}/{movies}/{shots}/{shot}/{image}"
+                        shutil.copyfile("cropped.jpg", f"/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/{movies}/image/Person/{name}/{name}_new_{image[:-4]}")
+                    except ValueError:
+                        print("No face was able to be matched")
+                        print(f"{img_path}/{movies}/{shots}/{shot}/{image}")
+                        break
+    
