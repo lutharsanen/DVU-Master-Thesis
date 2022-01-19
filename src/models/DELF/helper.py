cf6220a80817aa2221c1961e230777acfe4c79a6
lutharsanen <lutharsanen.kunam@uzh.ch>
2022-01-19 18:25:48 +0100

    finished location recognition pipeline

--- src/models/DELF/helper.py
+++ src/models/DELF/helper.py
@@ -4,9 +4,29 @@ from PIL import Image, ImageOps
 import numpy as np
 import tensorflow_hub as hub
 
+import numpy as np
+from scipy.spatial import cKDTree
+from skimage.measure import ransac
+from skimage.transform import AffineTransform
 
-def download_and_resize(name, path, new_width=256, new_height=256):
+from PIL import Image
+import imagehash
+
+def check_similarity(image1,image2):
+  hash0 = imagehash.average_hash(Image.open(image1)) 
+  hash1 = imagehash.average_hash(Image.open(image2)) 
+  cutoff = 30  # maximum bits that could be different between the hashes. 
+
+  if hash0 - hash1 < cutoff:
+    return True
+  else:
+    return False
+
+
+def download_and_resize(path, new_width=256, new_height=256):
   image = Image.open(path)
+  if np.array(image).shape[2] != 3:
+    image = image.convert('RGB')
   image = ImageOps.fit(image, (new_width, new_height), Image.ANTIALIAS)
   return image
 
@@ -20,3 +40,62 @@ def run_delf(image):
       score_threshold=tf.constant(100.0),
       image_scales=tf.constant([0.25, 0.3536, 0.5, 0.7071, 1.0, 1.4142, 2.0]),
       max_feature_num=tf.constant(1000))
+
+#@title TensorFlow is not needed for this post-processing and visualization
+def match_images(image1, image2):
+  if check_similarity(image1, image2):
+    image1 = download_and_resize(image1)
+    image2 = download_and_resize(image2)
+    result1 = run_delf(image1)
+    result2 = run_delf(image2)
+    distance_threshold = 0.8
+
+     # Read features.
+    num_features_1 = result1['locations'].shape[0]
+    #print("Loaded image 1's %d features" % num_features_1)
+
+    num_features_2 = result2['locations'].shape[0]
+    #print("Loaded image 2's %d features" % num_features_2)
+
+     # Find nearest-neighbor matches using a KD tree.
+    d1_tree = cKDTree(result1['descriptors'])
+    _, indices = d1_tree.query(
+        result2['descriptors'],
+        distance_upper_bound=distance_threshold)
+
+     # Select feature locations for putative matches.
+    locations_2_to_use = np.array([
+        result2['locations'][i,]
+        for i in range(num_features_2)
+        if indices[i] != num_features_1
+    ])
+    locations_1_to_use = np.array([
+        result1['locations'][indices[i],]
+        for i in range(num_features_2)
+        if indices[i] != num_features_1
+    ])
+
+    if (len(locations_2_to_use) and (len(locations_1_to_use))) > 3:
+        # Perform geometric verification using RANSAC.
+        _, inliers = ransac(
+            (locations_1_to_use, locations_2_to_use),
+            AffineTransform,
+            min_samples=3,
+            residual_threshold=20,
+            max_trials=1000)
+
+        if type(inliers) != np.ndarray:
+          print(inliers)
+          return False
+
+        print('Found %d inliers' % sum(inliers))
+        if sum(inliers) > 20:
+          return True
+        else:
+          return False
+    else:
+      return False
+
+  else:
+    return False
+
--- src/models/DELF/run.py
+++ src/models/DELF/run.py
@@ -1,78 +1,32 @@
-from absl import logging
 
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.spatial import cKDTree
-from skimage.feature import plot_matches
-from skimage.measure import ransac
-from skimage.transform import AffineTransform
-from six import BytesIO
+from helper import match_images
+import os
+from tqdm import tqdm
 
-import tensorflow as tf
-from helper import download_and_resize, run_delf
-
+def compare_location(image1, image_path, loc_path):
+    for location in os.listdir(loc_path):
+        for loc_image in os.listdir(f"{loc_path}/{location}"):
+            if match_images(f"{image_path}/{image1}",f"{loc_path}/{location}/{loc_image}"):
+                print(f"{image1} is in {location}.")
+                return
 
 
-image1 = download_and_resize('image_1.jpg', 'test1.jpg')
-image2 = download_and_resize('image_2.jpg', 'test2.jpg')
+movies = "honey"
+
+location_path = f"/media/lkunam/DVU-Challenge/HLVU/movie_knowledge_graph/{movies}/image/Location"
+img_path = "/media/lkunam/DVU-Challenge/HLVU/keyframes/shot_keyf"
 
 
-#@title TensorFlow is not needed for this post-processing and visualization
-def match_images(image1, image2, result1, result2):
-  distance_threshold = 0.8
-
-  # Read features.
-  num_features_1 = result1['locations'].shape[0]
-  print("Loaded image 1's %d features" % num_features_1)
-  
-  num_features_2 = result2['locations'].shape[0]
-  print("Loaded image 2's %d features" % num_features_2)
-
-  # Find nearest-neighbor matches using a KD tree.
-  d1_tree = cKDTree(result1['descriptors'])
-  _, indices = d1_tree.query(
-      result2['descriptors'],
-      distance_upper_bound=distance_threshold)
-
-  # Select feature locations for putative matches.
-  locations_2_to_use = np.array([
-      result2['locations'][i,]
-      for i in range(num_features_2)
-      if indices[i] != num_features_1
-  ])
-  locations_1_to_use = np.array([
-      result1['locations'][indices[i],]
-      for i in range(num_features_2)
-      if indices[i] != num_features_1
-  ])
-
-  # Perform geometric verification using RANSAC.
-  _, inliers = ransac(
-      (locations_1_to_use, locations_2_to_use),
-      AffineTransform,
-      min_samples=3,
-      residual_threshold=20,
-      max_trials=1000)
-
-  print('Found %d inliers' % sum(inliers))
-
-  # Visualize correspondences.
-  _, ax = plt.subplots()
-  inlier_idxs = np.nonzero(inliers)[0]
-  plot_matches(
-      ax,
-      image1,
-      image2,
-      locations_1_to_use,
-      locations_2_to_use,
-      np.column_stack((inlier_idxs, inlier_idxs)),
-      matches_color='b')
-  ax.axis('off')
-  ax.set_title('DELF correspondences')
-  plt.savefig("mygraph.png")
-
-result1 = run_delf(image1)
-result2 = run_delf(image2)
+shotlist = os.listdir(f"{img_path}/{movies}")
+orderedshotlist = [i.partition('-')[2] for i in shotlist]
+for i in tqdm(range(len(shotlist))):
+    num = i+1
+    list_index = orderedshotlist.index(str(num))
+    shots = shotlist[list_index]
+    for shot in os.listdir(f"{img_path}/{movies}/{shots}"):
+        border_list = []
+        for image in os.listdir(f"{img_path}/{movies}/{shots}/{shot}"):
+            ###########################################################
+            compare_location(image, f"{img_path}/{movies}/{shots}/{shot}", location_path)
 
 
-match_images(image1, image2, result1, result2)
